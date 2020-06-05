d1771cfa5b2c0c2671f42fc99e46a5f9207859c8
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2020-06-06 01:15:44 +0800

    Added styling for navigation bar

--- client/src/components/layout/NavigationBar.js
+++ client/src/components/layout/NavigationBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
 
 const NavigationBar = (props) => {
@@ -6,21 +7,21 @@ const NavigationBar = (props) => {
     <Nav
       activeKey='/home'
       onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+      className='parent-nav'
     >
-      <Nav.Item>
-        <Nav.Link href='/home'>Home</Nav.Link>
+      <Nav.Item className='nav-left'>
+        <Link to='/home'>Home</Link>
       </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey='link-1'>Register</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey='link-2'>Login</Nav.Link>
-      </Nav.Item>
-      {/* <Nav.Item>
-        <Nav.Link eventKey='disabled' disabled>
-          Disabled
-        </Nav.Link>
-      </Nav.Item> */}
+      <div className='nav-right'>
+        <Nav.Item>
+          <Link to='/register' className='register-nav'>
+            Register
+          </Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to='/login'>Login</Link>
+        </Nav.Item>
+      </div>
     </Nav>
   );
 };
