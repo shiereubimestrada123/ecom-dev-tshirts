f9a6fec4e77b58fbb80f8058f6836bba09f06ec7
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2020-06-05 22:52:25 +0800

    Added client repo and set up server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// Connect Database
+mongoose.connect(process.env.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('error', (error) => console.log(error));
+
+// Init Middleware
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
