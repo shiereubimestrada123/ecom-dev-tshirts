4ff27f5e88a7d789028197d8fcf96611b111f3c4
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2020-06-14 10:59:27 +0800

    Added auth middleware

---
+++ routes/api/auth.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    res.json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(express.json({ extended: false }));
 
 // Routes
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
