ee2b4c448dbfcbc29e2f497acfd3349368479dfb
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2020-06-25 17:49:33 +0800

    Added category form

--- client/src/pages/dashboard/admin/CreateCategory.js
+++ client/src/pages/dashboard/admin/CreateCategory.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
 
-const CreateCategory = (props) => {
-  return <div>Create Category Form</div>;
-};
+const CreateCategory = (props) => (
+  <Form className='my-5'>
+    <Form.Group controlId='formCategoryName'>
+      <Form.Label>Category Name</Form.Label>
+      <Form.Control
+        type='name'
+        placeholder='Enter category name'
+        name='name'
+        // value={email}
+        // onChange={(e) => onChange(e)}
+      />
+
+      <Button variant='info' type='submit' className='my-3'>
+        Submit
+      </Button>
+    </Form.Group>
+  </Form>
+);
 
 export default CreateCategory;
