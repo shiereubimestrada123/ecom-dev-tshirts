81214275dbce4f0e201ddb61c6f9365793da8bcf
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2020-06-30 14:15:27 +0800

    Added get categories on web server

--- routes/api/category.js
+++ routes/api/category.js
@@ -43,4 +43,14 @@ router.post(
   }
 );
 
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
