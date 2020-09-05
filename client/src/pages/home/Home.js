9989ca9f21b4efee82b0eacc480facb76e48920a
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2020-09-05 08:43:33 +0800

    Added image on homepage

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,16 +1,27 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import FormInput from '../../components/forms/forminput/FormInput';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { Row, Col, Spinner } from 'react-bootstrap';
 import { getCategories } from '../../store/actions/category';
+import { getProducts } from '../../store/actions/product';
 import { selectAuthLoading } from '../../store/selectors/auth';
+import { selectAllProducts } from '../../store/selectors/product';
 import AlertPrompt from '../../components/alertprompt/AlertPrompt';
+import home from '../../assets/images/home.jpg';
 
-const Home = ({ getCategories, loading }) => {
+const Home = ({ getCategories, getProducts, loading, products }) => {
   useEffect(() => {
     getCategories();
-  }, [getCategories]);
+    getProducts();
+    console.log(products);
+  }, [getCategories, getProducts]);
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
   return (
     <Fragment>
@@ -24,7 +35,18 @@ const Home = ({ getCategories, loading }) => {
               </Col>
             </Row>
           ) : (
-            <span>Home</span>
+            <div className='parent-home'>
+              <img src={home} />
+              <div class='parent-home-image'>
+                <FormInput
+                  name='test'
+                  id='test'
+                  className='btn btn-block home-btn'
+                  type='button'
+                  value='SHOP NOW'
+                />
+              </div>
+            </div>
           )}
         </Col>
       </Row>
@@ -34,10 +56,12 @@ const Home = ({ getCategories, loading }) => {
 
 Home.propTypes = {
   getCategories: PropTypes.func.isRequired,
+  getProducts: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
   loading: selectAuthLoading,
+  products: selectAllProducts,
 });
 
-export default connect(mapStateToProps, { getCategories })(Home);
+export default connect(mapStateToProps, { getCategories, getProducts })(Home);
