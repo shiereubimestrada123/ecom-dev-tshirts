15ad61fb393464e1434e76e5daac3cefdba31186
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2021-01-12 17:31:19 +0800

    Added responsiveness on footer

--- client/src/parts/footer/Footer.js
+++ client/src/parts/footer/Footer.js
@@ -1,44 +1,52 @@
 import React from 'react';
+import { Row, Col, Carousel } from 'react-bootstrap';
 
 const Footer = (props) => {
   return (
-    <footer className='flex-rw'>
-      <ul className='footer-list-top'>
-        <li>
-          <h4 className='footer-list-header'>Disclaimer</h4>
-        </li>
-        <li>
-          <span className='footer-list'>
-            1. This website was created solely for practice only.
-          </span>
-        </li>
-        <li>
-          <span className='footer-list'>
-            2. Materials such as images you will see is not owned by the creator
-            of this website.
-          </span>
-        </li>
-      </ul>
-      <ul className='footer-list-top'>
-        <li>
-          <h4 className='footer-list-header'>EcomDev Inc.</h4>
-        </li>
+    <Row className='footer-row'>
+      <footer className='flex-rw'>
+        <Col md={6}>
+          <ul className='footer-list-top'>
+            <li>
+              <h4 className='footer-list-header'>Disclaimer</h4>
+            </li>
+            <li>
+              <span className='footer-list'>
+                1. This website was created solely for practice only.
+              </span>
+            </li>
+            <li>
+              <span className='footer-list'>
+                2. Materials such as images you will see is not owned by the
+                creator of this website.
+              </span>
+            </li>
+          </ul>
+        </Col>
+        <Col md={6}>
+          <ul className='footer-list-top'>
+            <li>
+              <h4 className='footer-list-header'>EcomDev Inc.</h4>
+            </li>
 
-        <li className='footer-list'>
-          <i className='fas fa-envelope-square'></i> <span>asdasdasds</span>
-        </li>
-        <li className='footer-list'>
-          <i className='fas fa-map-marker-alt'></i> <span>1231231312</span>
-        </li>
-      </ul>
-
-      <section className='footer-bottom-section flex-rw'>
-        <div className='footer-bottom-wrapper'>
-          <i className='fas fa-copyright'></i> 2020 Copyright:{' '}
-          <span className='footer-bottom-rights'> EcomDev</span>
-        </div>
-      </section>
-    </footer>
+            <li className='footer-list'>
+              <i className='fas fa-envelope-square'></i> <span>asdasdasds</span>
+            </li>
+            <li className='footer-list'>
+              <i className='fas fa-map-marker-alt'></i> <span>1231231312</span>
+            </li>
+          </ul>
+        </Col>
+        <Col md={12}>
+          <section className='footer-bottom-section flex-rw'>
+            <div className='footer-bottom-wrapper'>
+              <i className='fas fa-copyright'></i> 2020 Copyright:{' '}
+              <span className='footer-bottom-rights'> EcomDev</span>
+            </div>
+          </section>
+        </Col>
+      </footer>
+    </Row>
   );
 };
 
