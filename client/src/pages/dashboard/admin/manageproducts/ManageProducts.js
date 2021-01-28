ba2b4a8588d3e7e1a49f759a25d1bc64a6b9995f
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2021-01-28 23:19:26 +0800

    Added pagination on manage product page

--- client/src/pages/dashboard/admin/manageproducts/ManageProducts.js
+++ client/src/pages/dashboard/admin/manageproducts/ManageProducts.js
@@ -1,9 +1,9 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { Spinner, Row, Col, Table } from 'react-bootstrap';
+import { Row, Col, Table } from 'react-bootstrap';
 import { Animated } from 'react-animated-css';
 import { getProducts, deleteProduct } from '../../../../store/actions/product';
 import { selectAllProducts } from '../../../../store/selectors/product';
@@ -13,6 +13,7 @@ import {
 } from '../../../../store/selectors/auth';
 import AlertPrompt from '../../../../components/alertprompt/AlertPrompt';
 import LoadingSpinner from '../../../../components/loadingspinner/LoadingSpinner';
+import PaginationProduct from '../../../../components/pagination/PaginationProduct';
 
 const ManageProducts = ({
   loading,
@@ -21,6 +22,15 @@ const ManageProducts = ({
   getProducts,
   deleteProduct,
 }) => {
+  const [currentpage, setcurrentpage] = useState(1);
+  const [productperpage] = useState(8);
+
+  const indexOfLastProduct = currentpage * productperpage;
+  const indexOfFirstProduct = indexOfLastProduct - productperpage;
+  const allProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const paginate = (pageNumber) => setcurrentpage(pageNumber);
+
   useEffect(() => {
     getProducts();
     window.scrollTo(0, 0);
@@ -57,8 +67,6 @@ const ManageProducts = ({
                       <h2>Manage Products</h2>
                     </div>
                     <div className='manage-body'>
-                      {/* <div className='manage-product-holder'>
-                        <h2>Total {products.length} products</h2> */}
                       <Table responsive='sm md lg xl'>
                         <thead>
                           <tr className='tr-header'>
@@ -68,8 +76,8 @@ const ManageProducts = ({
                           </tr>
                         </thead>
                         <tbody>
-                          {products.length > 0 &&
-                            products.map((product, index) => (
+                          {allProducts.length > 0 &&
+                            allProducts.map((product, index) => (
                               <tr key={index}>
                                 <td>{product.name}</td>
                                 <td onClick={() => handleRedirect(product._id)}>
@@ -86,49 +94,20 @@ const ManageProducts = ({
                             ))}
                         </tbody>
                       </Table>
-                      {/* </div> */}
+                      <div>
+                        <PaginationProduct
+                          productperpage={productperpage}
+                          totalproducts={products.length}
+                          paginate={paginate}
+                          currentpage={currentpage}
+                        />
+                      </div>
                     </div>
                   </div>
                 </section>
               </Col>
             </Row>
           </Animated>
-          {/* <Row className='mt-5 admin-row-header'>
-            <Col>
-              <i className='fab fa-black-tie' aria-hidden='true'></i> Manage
-              Products
-            </Col>
-          </Row>
-          <Row className='admin-row-body'>
-            <Col>
-              <div className='manage-product-holder'>
-                <h2>Total {products.length} products</h2>
-                <Table responsive='sm md lg xl' striped bordered>
-                  <thead>
-                    <tr>
-                      <th>Product Name</th>
-                      <th></th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {products.length > 0 &&
-                      products.map((product, index) => (
-                        <tr key={index}>
-                          <td>{product.name}</td>
-                          <td onClick={() => handleRedirect(product._id)}>
-                            <i className='fas fa-edit'></i>
-                          </td>
-                          <td onClick={() => handleDeleteProduct(product._id)}>
-                            <i className='fas fa-trash-alt'></i>
-                          </td>
-                        </tr>
-                      ))}
-                  </tbody>
-                </Table>
-              </div>
-            </Col>
-          </Row> */}
         </Fragment>
       )}
     </Fragment>
