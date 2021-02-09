599921b4774c748bbaf2020453046ff294b0bf79
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2021-02-10 02:22:24 +0800

    Modified add to cart page

--- client/src/pages/checkout/Checkout.js
+++ client/src/pages/checkout/Checkout.js
@@ -92,11 +92,11 @@ const Checkout = ({
               </Row>
 
               <Row>
-                <Col md={9}>
+                <Col md={8}>
                   <section className='holder-checkout-page'>
                     <div className='parent-checkout-page'>
                       <div className='checkout-heading'>
-                        <h2>Delivery Address</h2>
+                        <h3>Delivery Address</h3>
                       </div>
                       <div className='checkout-body'>
                         <Form className='checkout-form-parent'>
@@ -168,13 +168,13 @@ const Checkout = ({
                     </div>
                   </section>
                 </Col>
-                <Col md={3}>
+                <Col md={4}>
                   <section className='holder-order-details'>
                     <Table responsive='sm md lg xl'>
                       <thead>
                         <tr className='tr-header'>
                           <th colSpan='2' className='product-name'>
-                            Order Details
+                            <h3>Order Details</h3>
                           </th>
                         </tr>
                       </thead>
@@ -202,142 +202,8 @@ const Checkout = ({
                       </Button>
                     </div>
                   </section>
-                  {/* <div className='holder-payment'>
-                    <h3>Order Details</h3>
-                    <div className='checkout-product-count'>
-                      <span> # item(s)</span>
-                      <span>{productCount}</span>
-                    </div>
-                    <div className='checkout-total'>
-                      <span>Total</span>
-                      <span>&#8369;{total}</span>
-                    </div>
-                    {productCount === 0 && total === 0 ? (
-                      <div className='holder-place-order-button'>
-                        <FormInput
-                          className='btn btn-block place-order-btn'
-                          type='submit'
-                          value='Shop'
-                          onClick={() => history.push('/shop')}
-                        />
-                      </div>
-                    ) : (
-                      <Fragment>
-                        <div className='holder-place-order-button'>
-                          <FormInput
-                            className='btn btn-block place-order-btn'
-                            type='submit'
-                            value='Place order'
-                            onClick={buy}
-                          />
-                        </div>
-                      </Fragment>
-                    )}
-                  </div> */}
                 </Col>
               </Row>
-
-              {/* <Row>
-                <Col>
-                  <AlertPrompt />
-                  <h1 className='heading-checkout'>Secure Checkout</h1>
-                </Col>
-              </Row>
-              <Row>
-                <Col md={9}>
-                  <div className='checkout-wrapper'>
-                    <Form className='checkout-form-parent'>
-                      <Form.Group className='form-group'>
-                        <Form.Label className='form-label'>Name</Form.Label>
-                        <FormInput
-                          type='text'
-                          placeholder='Enter name'
-                          name='name'
-                          value={name}
-                          onChange={(e) => onChange(e)}
-                          className='form-control'
-                        />
-                      </Form.Group>
-
-                      <Form.Group className='form-group'>
-                        <Form.Label className='form-label'>
-                          Email address
-                        </Form.Label>
-                        <FormInput
-                          type='email'
-                          placeholder='Enter email'
-                          name='email'
-                          value={email}
-                          onChange={(e) => onChange(e)}
-                          className='form-control'
-                        />
-                      </Form.Group>
-
-                      <Form.Group className='form-group'>
-                        <Form.Label className='form-label'>Address</Form.Label>
-
-                        <FormInput
-                          type='text'
-                          placeholder='Enter address'
-                          name='address'
-                          value={address}
-                          onChange={(e) => onChange(e)}
-                          className='form-control'
-                        />
-                      </Form.Group>
-
-                      <Form.Group className='form-group'>
-                        <Form.Label className='form-label'>
-                          Contact number
-                        </Form.Label>
-
-                        <FormInput
-                          type='number'
-                          placeholder='Enter contact number'
-                          name='contact'
-                          value={contact}
-                          onChange={(e) => onChange(e)}
-                          className='form-control'
-                        />
-                      </Form.Group>
-                    </Form>
-                  </div>
-                </Col>
-                <Col md={3}>
-                  <div className='holder-payment'>
-                    <h3>Order Details</h3>
-                    <div className='checkout-product-count'>
-                      <span> # item(s)</span>
-                      <span>{productCount}</span>
-                    </div>
-                    <div className='checkout-total'>
-                      <span>Total</span>
-                      <span>&#8369;{total}</span>
-                    </div>
-                    {productCount === 0 && total === 0 ? (
-                      <div className='holder-place-order-button'>
-                        <FormInput
-                          className='btn btn-block place-order-btn'
-                          type='submit'
-                          value='Shop'
-                          onClick={() => history.push('/shop')}
-                        />
-                      </div>
-                    ) : (
-                      <Fragment>
-                        <div className='holder-place-order-button'>
-                          <FormInput
-                            className='btn btn-block place-order-btn'
-                            type='submit'
-                            value='Place order'
-                            onClick={buy}
-                          />
-                        </div>
-                      </Fragment>
-                    )}
-                  </div>
-                </Col>
-              </Row> */}
             </Animated>
           </Fragment>
         )}
