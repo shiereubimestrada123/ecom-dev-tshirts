bafe8c0892ec90925b0819ae80e1cc219faa4961
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2021-02-13 01:30:39 +0800

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -32,6 +33,16 @@ app.use('/api/category', require('./routes/api/category'));
 app.use('/api/product', require('./routes/api/product'));
 app.use('/api/order', require('./routes/api/order'));
 app.use('/api/mail', require('./routes/api/mail'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
