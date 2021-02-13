e30c310bdb8e26fac3db3808e2b91fa163504189
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2021-02-13 18:25:05 +0800

    Prepared for deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 // Connect Database
 try {
-  mongoose.connect(process.env.mongoURI, {
+  mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
