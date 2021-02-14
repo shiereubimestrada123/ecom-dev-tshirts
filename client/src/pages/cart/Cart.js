41442cd16fe6b5af7cc1310453633b99102604d4
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2021-02-15 00:14:53 +0800

    Modified cart page

--- client/src/pages/cart/Cart.js
+++ client/src/pages/cart/Cart.js
@@ -42,6 +42,10 @@ const Cart = ({
     addProductCart(product);
   };
 
+  const handleOnclick = () => {
+    history.push('/shop');
+  };
+
   const isButton = () => {
     if (cartProducts.length > 0) {
       if (isAuthenticated) {
@@ -87,80 +91,90 @@ const Cart = ({
               </Col>
             </Row>
 
-            <Table responsive='sm md lg xl'>
-              <thead>
-                <tr className='tr-header'>
-                  <th>Image</th>
-                  <th className='mobile-hide'>Name</th>
-                  <th className='mobile-hide'>Quantity</th>
-                  <th>Price</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody className='tbody-cart'>
-                {cartProducts.length > 0 ? (
-                  cartProducts.map((product, index) => (
-                    <tr key={index}>
-                      <Fragment>
-                        <td>
-                          <Card className='cart-card'>
-                            <CardTemplate
-                              product={product}
-                              src={`/api/product/photo/${product._id}`}
-                              classImage='cart-image'
-                              variant='top'
-                            />
-                          </Card>
-                        </td>
-                        <td className='mobile-hide'>{product.name}</td>
-                        <td className='mobile-hide'>
-                          {product.count > 1 ? (
-                            <span
-                              className='decrease-quantity'
-                              onClick={() => decreaseProductCart(product)}
-                            >
-                              &#65308;
-                            </span>
-                          ) : (
-                            <span
-                              className='decrease-quantity'
-                              onClick={() => clearProductCart(product)}
-                            >
-                              &#65308;
-                            </span>
-                          )}{' '}
-                          {product.count}{' '}
-                          <span
-                            className='increase-quantity'
-                            onClick={() => addQuantity(product)}
-                          >
-                            &#65310;
-                          </span>
-                        </td>
-                        <td>{product.price}</td>
-                        <td onClick={() => clearProductCart(product)}>
-                          <i className='fas fa-trash-alt'></i>
-                        </td>
-                      </Fragment>
-                    </tr>
-                  ))
-                ) : (
-                  <tr>
-                    <td colSpan='5' className='empty-cart'>
-                      You have no existing item, please{' '}
-                      <Link to='/shop'>Shop</Link> first
-                    </td>
+            {cartProducts.length > 0 ? (
+              <Table responsive='sm md lg xl'>
+                <thead>
+                  <tr className='tr-header'>
+                    <th>Image</th>
+                    <th className='mobile-hide'>Name</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th></th>
                   </tr>
-                )}
-                <tr>
-                  <td className='no-border-right-td'>Total</td>
-                  <td className='no-border-td mobile-hide'></td>
-                  <td className='no-border-td mobile-hide'></td>
-                  <td className='no-border-td'></td>
-                  <td>{total}</td>
-                </tr>
-              </tbody>
-            </Table>
+                </thead>
+                <tbody className='tbody-cart'>
+                  {cartProducts.length > 0 &&
+                    cartProducts.map((product, index) => (
+                      <tr key={index}>
+                        <Fragment>
+                          <td>
+                            <Card className='cart-card'>
+                              <CardTemplate
+                                product={product}
+                                src={`/api/product/photo/${product._id}`}
+                                classImage='cart-image'
+                                variant='top'
+                              />
+                            </Card>
+                          </td>
+                          <td className='mobile-hide'>{product.name}</td>
+                          <td>
+                            {product.count > 1 ? (
+                              <span
+                                className='decrease-quantity'
+                                onClick={() => decreaseProductCart(product)}
+                              >
+                                &#65308;
+                              </span>
+                            ) : (
+                              <span
+                                className='decrease-quantity'
+                                onClick={() => clearProductCart(product)}
+                              >
+                                &#65308;
+                              </span>
+                            )}{' '}
+                            {product.count}{' '}
+                            <span
+                              className='increase-quantity'
+                              onClick={() => addQuantity(product)}
+                            >
+                              &#65310;
+                            </span>
+                          </td>
+                          <td>{product.price}</td>
+                          <td onClick={() => clearProductCart(product)}>
+                            <i className='fas fa-trash-alt'></i>
+                          </td>
+                        </Fragment>
+                      </tr>
+                    ))}
+                  <tr>
+                    <td className='no-border-right-td'>Total</td>
+                    <td className='no-border-td mobile-hide'></td>
+                    <td className='no-border-td '></td>
+                    <td className='no-border-td'></td>
+                    <td>{total}</td>
+                  </tr>
+                </tbody>
+              </Table>
+            ) : (
+              <Row>
+                <Col md={12}>
+                  <div className='empty-cart'>
+                    <h5>You have no existing item please shop first</h5>
+                    <Button
+                      variant='secondary'
+                      className='button-goback shadow-none'
+                      type='submit'
+                      onClick={handleOnclick}
+                    >
+                      Shop
+                    </Button>
+                  </div>
+                </Col>
+              </Row>
+            )}
             <div className='proceed-checkout'>{isButton()}</div>
           </Animated>
         </Fragment>
